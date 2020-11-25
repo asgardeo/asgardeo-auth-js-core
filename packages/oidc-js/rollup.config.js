7361d5ad456d1fcadf32360a2af2cb90e2e0095c
Thivi <theviyanthan@outlook.com>
2020-11-26 01:30:31 +0530

    Add polyfilled bundles

--- packages/oidc-js/rollup.config.js
+++ packages/oidc-js/rollup.config.js
@@ -22,6 +22,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import eslint from "@rollup/plugin-eslint";
 import resolve from "@rollup/plugin-node-resolve";
 import analyze from "rollup-plugin-analyzer"
+import injectProcessEnv from "rollup-plugin-inject-process-env";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
@@ -41,10 +42,41 @@ export default [
                 preferBuiltins: true
             }),
             commonjs(),
+            injectProcessEnv({
+                NODE_ENV: "production"
+            }),
             eslint(),
             typescript(),
             workerLoader({
                 extensions: [".ts"],
+                sourcemap: false,
+                targetPlatform: "browser"
+            }),
+            terser(),
+            analyze({ limit: 10 }),
+            //sourcemaps()
+        ]
+    },
+
+    {
+        input: "src/index-polyfill.ts",
+        output: {
+            file: pkg.module.split("/").shift() + "/polyfilled/" + pkg.module.split("/").pop(),
+            format: "esm"
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                preferBuiltins: true
+            }),
+            commonjs(),
+            injectProcessEnv({
+                NODE_ENV: "production"
+            }),
+            eslint(),
+            typescript(),
+            workerLoader({
+                extensions: [ ".ts" ],
                 sourcemap: false,
                 targetPlatform: "browser"
             }),
@@ -61,9 +93,9 @@ export default [
         ]
     },
     {
-        input: "src/index.ts",
+        input: "src/index-polyfill.ts",
         output: {
-            file: pkg.main,
+            file: pkg.main.split("/").shift() + "/polyfilled/" + pkg.main.split("/").pop(),
             format: "umd",
             name: "AsgardioOIDC"
         },
@@ -94,7 +126,57 @@ export default [
     {
         input: "src/index.ts",
         output: {
+            file: pkg.main,
+            format: "umd",
+            name: "AsgardioOIDC"
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                preferBuiltins: true
+            }),
+            commonjs(),
+            eslint(),
+            typescript(),
+            workerLoader({
+                extensions: [ ".ts" ],
+                sourcemap: false,
+                targetPlatform: "browser"
+            }),
+            terser()
+        ]
+    },
+    {
+        input: "src/index.ts",
+        output: {
             file: "dist/asgardio-oidc.production.min.js",
+            format: "iife",
+            name: "AsgardioOIDC"
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                preferBuiltins: true
+            }),
+            commonjs(),
+            eslint(),
+            typescript(),
+            workerLoader({
+                extensions: [ ".ts" ],
+                sourcemap: false,
+                targetPlatform: "browser"
+            }),
+            terser({
+                output: {
+                    comments: false
+                }
+            })
+        ]
+    },
+    {
+        input: "src/index-polyfill.ts",
+        output: {
+            file: "dist/polyfilled/asgardio-oidc.production.min.js",
             format: "iife",
             name: "AsgardioOIDC"
         },
