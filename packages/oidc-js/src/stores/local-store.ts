56518b879d856ae9e31c9fafdf2cfa8befd6672f
Thivi <theviyanthan@outlook.com>
2020-12-16 15:56:59 +0530

    Simplify local store

--- packages/oidc-js/src/stores/local-store.ts
+++ packages/oidc-js/src/stores/local-store.ts
@@ -20,148 +20,15 @@ import { ConfigInterface, OIDCProviderMetaData, SessionDataRaw } from "../models
 import { Stores } from "../constants";
 
 export class LocalStore implements Store {
-    private setDataInBulk(
-        key: Stores,
-        data: ConfigInterface | OIDCProviderMetaData | SessionDataRaw | TemporaryData
-    ): void {
-        const existingDataJSON = localStorage.getItem(key);
-        const existingData = existingDataJSON && JSON.parse(existingDataJSON);
-
-        const dataToBeSaved = { ...existingData, ...data };
-        const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
-        localStorage.setItem(key, dataToBeSavedJSON);
+    public setData(key: string, value: string): void {
+        localStorage.setItem(key, value);
     }
 
-    private setValue(
-        key: Stores,
-        attribute: keyof ConfigInterface | keyof OIDCProviderMetaData | keyof SessionDataRaw | keyof TemporaryData,
-        value: StoreValue
-    ): void {
-        const existingDataJSON = localStorage.getItem(key);
-        const existingData = existingDataJSON && JSON.parse(existingDataJSON);
-
-        const dataToBeSaved = { ...existingData, [attribute]: value };
-        const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
-
-        localStorage.setItem(key, dataToBeSavedJSON);
+    public getData(key: string): string {
+        return localStorage.getItem(key);
     }
 
-    private removeValue(
-        key: Stores,
-        attribute: keyof ConfigInterface | keyof OIDCProviderMetaData | keyof SessionDataRaw | keyof TemporaryData
-    ): void {
-        const existingDataJSON = localStorage.getItem(key);
-        const existingData = existingDataJSON && JSON.parse(existingDataJSON);
-
-        const dataToBeSaved = { ...existingData };
-        delete dataToBeSaved[attribute];
-        const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
-
-        localStorage.setItem(key, dataToBeSavedJSON);
-    }
-
-    private;
-    public setConfigData(config: ConfigInterface): void {
-        this.setDataInBulk(Stores.ConfigData, config);
-    }
-
-    public setOIDCProviderMetaData(oidcProviderMetaData: OIDCProviderMetaData): void {
-        this.setDataInBulk(Stores.OIDCProviderMetaData, oidcProviderMetaData);
-    }
-
-    public setTemporaryData(temporaryData: TemporaryData): void {
-        this.setDataInBulk(Stores.TemporaryData, temporaryData);
-    }
-
-    public setSessionData(sessionData: SessionDataRaw): void {
-        this.setDataInBulk(Stores.SessionData, sessionData);
-    }
-
-    public getConfigData(): ConfigInterface {
-        return JSON.parse(localStorage.getItem(Stores.ConfigData));
-    }
-
-    public getOIDCProviderMetaData(): OIDCProviderMetaData {
-        return JSON.parse(localStorage.getItem(Stores.OIDCProviderMetaData));
-    }
-
-    public getTemporaryData(): TemporaryData {
-        return JSON.parse(localStorage.getItem(Stores.TemporaryData));
-    }
-
-    public getSessionData(): SessionDataRaw {
-        return JSON.parse(localStorage.getItem(Stores.SessionData));
-    }
-
-    public removeConfigData(): void {
-        localStorage.removeItem(Stores.ConfigData);
-    }
-
-    public removeOIDCProviderMetaData(): void {
-        localStorage.removeItem(Stores.OIDCProviderMetaData);
-    }
-
-    public removeTemporaryData(): void {
-        localStorage.removeItem(Stores.TemporaryData);
-    }
-
-    public removeSessionData(): void {
-        localStorage.removeItem(Stores.SessionData);
-    }
-
-    public getConfigDataParameter(key: keyof ConfigInterface): StoreValue {
-        return localStorage.getItem(Stores.ConfigData) && JSON.parse(localStorage.getItem(Stores.ConfigData))[key];
-    }
-
-    public getOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): StoreValue {
-        return (
-            localStorage.getItem(Stores.OIDCProviderMetaData) &&
-            JSON.parse(localStorage.getItem(Stores.OIDCProviderMetaData))[key]
-        );
-    }
-
-    public getTemporaryDataParameter(key: keyof TemporaryData): StoreValue {
-        return (
-            localStorage.getItem(Stores.TemporaryData) &&
-            JSON.parse(localStorage.getItem(Stores.TemporaryData))[key]
-        );
-    }
-
-    public getSessionDataParameter(key: keyof SessionDataRaw): StoreValue {
-        return (
-            localStorage.getItem(Stores.SessionData) && JSON.parse(localStorage.getItem(Stores.SessionData))[key]
-        );
-    }
-
-    public setConfigDataParameter(key: keyof ConfigInterface, value: StoreValue): void {
-        this.setValue(Stores.ConfigData, key, value);
-    }
-
-    public setOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData, value: StoreValue): void {
-        this.setValue(Stores.OIDCProviderMetaData, key, value);
-    }
-
-    public setTemporaryDataParameter(key: keyof TemporaryData, value: StoreValue): void {
-        this.setValue(Stores.TemporaryData, key, value);
-    }
-
-    public setSessionDataParameter(key: keyof SessionDataRaw, value: StoreValue): void {
-        this.setValue(Stores.SessionData, key, value);
-    }
-
-    public removeConfigDataParameter(key: keyof ConfigInterface): void {
-        this.removeValue(Stores.ConfigData, key);
-    }
-
-    public removeOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): void {
-        this.removeValue(Stores.OIDCProviderMetaData, key);
-    }
-
-    public removeTemporaryDataParameter(key: keyof TemporaryData): void {
-        this.removeValue(Stores.TemporaryData, key);
-    }
-
-    public removeSessionDataParameter(key: keyof SessionDataRaw): void {
-        this.removeValue(Stores.SessionData, key);
+    public removeData(key: string): void {
+        localStorage.removeItem(key);
     }
 }
