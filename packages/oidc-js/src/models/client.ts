25306f300aed2e78080cd012e36ad877dd80984e
Thivi <theviyanthan@outlook.com>
2020-12-18 14:43:00 +0530

    Remove BasicConfig

--- packages/oidc-js/src/models/client.ts
+++ packages/oidc-js/src/models/client.ts
@@ -16,59 +16,27 @@
  * under the License.
  */
 
-import { OIDCEndpoints } from "../core/models/oidc-provider-meta-data";
-import { SessionData } from "./web-worker-client";
-import { ResponseMode, Storage } from "../constants";
+import { Storage } from "../constants";
 import { HttpError, HttpResponse } from "../models";
-
-interface BaseConfigInterface {
-    authorizationType?: string;
-    signInRedirectURL: string;
-    signOutRedirectURL?: string;
-    clientHost?: string;
-    clientID: string;
-    clientSecret?: string;
-    consentDenied?: boolean;
-    enablePKCE?: boolean;
-    prompt?: string;
-    responseMode?: ResponseMode;
-    scope?: string[];
-    serverOrigin: string;
-    endpoints?: OIDCEndpoints;
-    overrideWellEndpointConfig?: boolean;
-    wellKnownEndpoint?: string;
-    authorizationCode?: string;
-    sessionState?: string;
-    validateIDToken?: boolean;
-    /**
-     * Allowed leeway for id_tokens (in seconds).
-     */
-    clockTolerance?: number;
-}
 
 /**
  * SDK Client config parameters.
  */
-export interface NonWebWorkerConfigInterface extends BaseConfigInterface{
+export interface MainThreadClientConfig {
     storage?: Storage.SessionStorage | Storage.LocalStorage | Storage.MainThreadMemory;
 }
 
-export interface WebWorkerConfigInterface extends BaseConfigInterface {
+export interface WebWorkerClientConfig {
     resourceServerURLs: string[];
-    session?: SessionData;
     storage: Storage.WebWorker;
     requestTimeout?: number;
 }
 
-export type ConfigInterface = NonWebWorkerConfigInterface | WebWorkerConfigInterface;
+export type ConfigInterface = MainThreadClientConfig | WebWorkerClientConfig;
 
 export interface HttpClient {
     requestStartCallback: () => void;
     requestSuccessCallback: (response: HttpResponse) => void;
     requestErrorCallback: (error: HttpError) => void;
     requestFinishCallback: () => void;
-}
-
-export interface WebWorkerClientConfigInterface extends WebWorkerConfigInterface {
-    httpClient: HttpClient;
 }
