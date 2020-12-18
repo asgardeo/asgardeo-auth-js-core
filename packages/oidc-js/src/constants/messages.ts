b8202e5775ccadcc64284c14519a300d3c667da9
Thivi <theviyanthan@outlook.com>
2020-12-18 13:47:09 +0530

    Remove logout success

--- packages/oidc-js/src/constants/messages.ts
+++ packages/oidc-js/src/constants/messages.ts
@@ -34,7 +34,6 @@ export const REQUEST_FINISH = "request-finish";
 export const GET_SERVICE_ENDPOINTS = "get-service-endpoints";
 export const GET_USER_INFO = "get-user-info";
 export const GET_DECODED_ID_TOKEN = "get-decoded-id-token";
-export const LOGOUT_SUCCESS = "logout_success";
 export const ENABLE_HTTP_HANDLER = "enable_http_handler";
 export const DISABLE_HTTP_HANDLER = "disable_http_handler";
 export const GET_AUTH_URL = "get_auth_url";
