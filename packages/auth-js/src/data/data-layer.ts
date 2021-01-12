8b2cbab3e14d0f1dc09372c6ce54647e86a55d14
Thivi <theviyanthan@outlook.com>
2021-01-12 17:35:14 +0530

    Convert data layer methods to async functions

--- packages/auth-js/src/data/data-layer.ts
+++ packages/auth-js/src/data/data-layer.ts
@@ -27,154 +27,150 @@ export class DataLayer<T> {
         this._store = store;
     }
 
-    private setDataInBulk(
+    private async setDataInBulk(
         key: string,
         data: Partial<AuthClientConfig<T> | OIDCProviderMetaData | SessionData | TemporaryData>
-    ): void {
-        const existingDataJSON = this._store.getData(key) ?? null;
+    ): Promise<void> {
+        const existingDataJSON = (await this._store.getData(key)) ?? null;
         const existingData = existingDataJSON && JSON.parse(existingDataJSON);
 
         const dataToBeSaved = { ...existingData, ...data };
         const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
-        this._store.setData(key, dataToBeSavedJSON);
+        await this._store.setData(key, dataToBeSavedJSON);
     }
 
-    private setValue(
+    private async setValue(
         key: string,
         attribute: keyof AuthClientConfig<T> | keyof OIDCProviderMetaData | keyof SessionData | keyof TemporaryData,
         value: StoreValue
-    ): void {
-        const existingDataJSON = this._store.getData(key) ?? null;
+    ): Promise<void> {
+        const existingDataJSON = (await this._store.getData(key)) ?? null;
         const existingData = existingDataJSON && JSON.parse(existingDataJSON);
 
         const dataToBeSaved = { ...existingData, [attribute]: value };
         const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
 
-        this._store.setData(key, dataToBeSavedJSON);
+        await this._store.setData(key, dataToBeSavedJSON);
     }
 
-    private removeValue(
+    private async removeValue(
         key: string,
         attribute: keyof AuthClientConfig<T> | keyof OIDCProviderMetaData | keyof SessionData | keyof TemporaryData
-    ): void {
-        const existingDataJSON = this._store.getData(key) ?? null;
+    ): Promise<void> {
+        const existingDataJSON = (await this._store.getData(key)) ?? null;
         const existingData = existingDataJSON && JSON.parse(existingDataJSON);
 
         const dataToBeSaved = { ...existingData };
         delete dataToBeSaved[attribute];
         const dataToBeSavedJSON = JSON.stringify(dataToBeSaved);
-        this._store.setData(key, dataToBeSavedJSON);
+        await this._store.setData(key, dataToBeSavedJSON);
     }
 
     private _resolveKey(store: Stores): string {
         return `${store}-${this._id}`;
     }
 
-    public setConfigData(config: Partial<AuthClientConfig<T>>): void {
-        this.setDataInBulk(this._resolveKey(Stores.ConfigData), config);
+    public async setConfigData(config: Partial<AuthClientConfig<T>>): Promise<void> {
+        await this.setDataInBulk(this._resolveKey(Stores.ConfigData), config);
     }
 
-    public setOIDCProviderMetaData(oidcProviderMetaData: Partial<OIDCProviderMetaData>): void {
+    public async setOIDCProviderMetaData(oidcProviderMetaData: Partial<OIDCProviderMetaData>): Promise<void> {
         this.setDataInBulk(this._resolveKey(Stores.OIDCProviderMetaData), oidcProviderMetaData);
     }
 
-    public setTemporaryData(temporaryData: Partial<TemporaryData>): void {
+    public async setTemporaryData(temporaryData: Partial<TemporaryData>): Promise<void> {
         this.setDataInBulk(this._resolveKey(Stores.TemporaryData), temporaryData);
     }
 
-    public setSessionData(sessionData: Partial<SessionData>): void {
+    public async setSessionData(sessionData: Partial<SessionData>): Promise<void> {
         this.setDataInBulk(this._resolveKey(Stores.SessionData), sessionData);
     }
 
-    public getConfigData(): AuthClientConfig<T> {
-        return JSON.parse(this._store.getData(this._resolveKey(Stores.ConfigData)) ?? null);
+    public async getConfigData(): Promise<AuthClientConfig<T>> {
+        return JSON.parse((await this._store.getData(this._resolveKey(Stores.ConfigData))) ?? null);
     }
 
-    public getOIDCProviderMetaData(): OIDCProviderMetaData {
-        return JSON.parse(this._store.getData(this._resolveKey(Stores.OIDCProviderMetaData)) ?? null);
+    public async getOIDCProviderMetaData(): Promise<OIDCProviderMetaData> {
+        return JSON.parse((await this._store.getData(this._resolveKey(Stores.OIDCProviderMetaData))) ?? null);
     }
 
-    public getTemporaryData(): TemporaryData {
-        return JSON.parse(this._store.getData(this._resolveKey(Stores.TemporaryData)) ?? null);
+    public async getTemporaryData(): Promise<TemporaryData> {
+        return JSON.parse((await this._store.getData(this._resolveKey(Stores.TemporaryData))) ?? null);
     }
 
-    public getSessionData(): SessionData {
-        return JSON.parse(this._store.getData(this._resolveKey(Stores.SessionData)) ?? null);
+    public async getSessionData(): Promise<SessionData> {
+        return JSON.parse((await this._store.getData(this._resolveKey(Stores.SessionData))) ?? null);
     }
 
-    public removeConfigData(): void {
-        this._store.removeData(this._resolveKey(Stores.ConfigData));
+    public async removeConfigData(): Promise<void> {
+        await this._store.removeData(this._resolveKey(Stores.ConfigData));
     }
 
-    public removeOIDCProviderMetaData(): void {
-        this._store.removeData(this._resolveKey(Stores.OIDCProviderMetaData));
+    public async removeOIDCProviderMetaData(): Promise<void> {
+        await this._store.removeData(this._resolveKey(Stores.OIDCProviderMetaData));
     }
 
-    public removeTemporaryData(): void {
-        this._store.removeData(this._resolveKey(Stores.TemporaryData));
+    public async removeTemporaryData(): Promise<void> {
+        await this._store.removeData(this._resolveKey(Stores.TemporaryData));
     }
 
-    public removeSessionData(): void {
-        this._store.removeData(this._resolveKey(Stores.SessionData));
+    public async removeSessionData(): Promise<void> {
+        await this._store.removeData(this._resolveKey(Stores.SessionData));
     }
 
-    public getConfigDataParameter(key: keyof AuthClientConfig<T>): StoreValue {
-        return (
-            this._store.getData(this._resolveKey(Stores.ConfigData)) &&
-            JSON.parse(this._store.getData(this._resolveKey(Stores.ConfigData)) ?? null)[key]
-        );
+    public async getConfigDataParameter(key: keyof AuthClientConfig<T>): Promise<StoreValue> {
+        const data = await this._store.getData(this._resolveKey(Stores.ConfigData));
+
+        return data && JSON.parse(data)[key];
     }
 
-    public getOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): StoreValue {
-        return (
-            this._store.getData(this._resolveKey(Stores.OIDCProviderMetaData)) &&
-            JSON.parse(this._store.getData(this._resolveKey(Stores.OIDCProviderMetaData)) ?? null)[key]
-        );
+    public async getOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): Promise<StoreValue> {
+        const data = await this._store.getData(this._resolveKey(Stores.OIDCProviderMetaData));
+
+        return data && JSON.parse(data)[key];
     }
 
-    public getTemporaryDataParameter(key: keyof TemporaryData): StoreValue {
-        return (
-            this._store.getData(this._resolveKey(Stores.TemporaryData)) &&
-            JSON.parse(this._store.getData(this._resolveKey(Stores.TemporaryData)) ?? null)[key]
-        );
+    public async getTemporaryDataParameter(key: keyof TemporaryData): Promise<StoreValue> {
+        const data = await this._store.getData(this._resolveKey(Stores.TemporaryData));
+
+        return data && JSON.parse(data)[key];
     }
 
-    public getSessionDataParameter(key: keyof SessionData): StoreValue {
-        return (
-            this._store.getData(this._resolveKey(Stores.SessionData)) &&
-            JSON.parse(this._store.getData(this._resolveKey(Stores.SessionData)) ?? null)[key]
-        );
+    public async getSessionDataParameter(key: keyof SessionData): Promise<StoreValue> {
+        const data = await this._store.getData(this._resolveKey(Stores.SessionData));
+
+        return data && JSON.parse(data)[key];
     }
 
-    public setConfigDataParameter(key: keyof AuthClientConfig<T>, value: StoreValue): void {
-        this.setValue(this._resolveKey(Stores.ConfigData), key, value);
+    public async setConfigDataParameter(key: keyof AuthClientConfig<T>, value: StoreValue): Promise<void> {
+        await this.setValue(this._resolveKey(Stores.ConfigData), key, value);
     }
 
-    public setOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData, value: StoreValue): void {
-        this.setValue(this._resolveKey(Stores.OIDCProviderMetaData), key, value);
+    public async setOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData, value: StoreValue): Promise<void> {
+        await this.setValue(this._resolveKey(Stores.OIDCProviderMetaData), key, value);
     }
 
-    public setTemporaryDataParameter(key: keyof TemporaryData, value: StoreValue): void {
-        this.setValue(this._resolveKey(Stores.TemporaryData), key, value);
+    public async setTemporaryDataParameter(key: keyof TemporaryData, value: StoreValue): Promise<void> {
+        await this.setValue(this._resolveKey(Stores.TemporaryData), key, value);
     }
 
-    public setSessionDataParameter(key: keyof SessionData, value: StoreValue): void {
-        this.setValue(this._resolveKey(Stores.SessionData), key, value);
+    public async setSessionDataParameter(key: keyof SessionData, value: StoreValue): Promise<void> {
+        await this.setValue(this._resolveKey(Stores.SessionData), key, value);
     }
 
-    public removeConfigDataParameter(key: keyof AuthClientConfig<T>): void {
-        this.removeValue(this._resolveKey(Stores.ConfigData), key);
+    public async removeConfigDataParameter(key: keyof AuthClientConfig<T>): Promise<void> {
+        await this.removeValue(this._resolveKey(Stores.ConfigData), key);
     }
 
-    public removeOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): void {
-        this.removeValue(this._resolveKey(Stores.OIDCProviderMetaData), key);
+    public async removeOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): Promise<void> {
+        await this.removeValue(this._resolveKey(Stores.OIDCProviderMetaData), key);
     }
 
-    public removeTemporaryDataParameter(key: keyof TemporaryData): void {
-        this.removeValue(this._resolveKey(Stores.TemporaryData), key);
+    public async removeTemporaryDataParameter(key: keyof TemporaryData): Promise<void> {
+        await this.removeValue(this._resolveKey(Stores.TemporaryData), key);
     }
 
-    public removeSessionDataParameter(key: keyof SessionData): void {
-        this.removeValue(this._resolveKey(Stores.SessionData), key);
+    public async removeSessionDataParameter(key: keyof SessionData): Promise<void> {
+        await this.removeValue(this._resolveKey(Stores.SessionData), key);
     }
 }
