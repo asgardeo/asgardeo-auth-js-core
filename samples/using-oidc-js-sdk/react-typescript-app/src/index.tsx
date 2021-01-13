a4a8cdb22d36520ec3cd8fc02be76f9c444292a7
Thivi <theviyanthan@outlook.com>
2021-01-13 20:20:23 +0530

    Fix conflicts

--- samples/using-oidc-js-sdk/react-typescript-app/src/index.tsx
+++ samples/using-oidc-js-sdk/react-typescript-app/src/index.tsx
@@ -33,12 +33,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-    useEffect(() => {
-        const auth = AsgardeoSPAClient.getInstance();
-
-        auth.signOut();
-    }, [ history ]);
-
-    return null;
-};
-reportWebVitals();
