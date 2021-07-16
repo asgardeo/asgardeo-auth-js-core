ae7a1a57c76715ddea23ec6001d92dd195d121e9
Thivi <theviyanthan@outlook.com>
2021-07-16 23:20:21 +0530

    Remove redundant data interface

--- lib/src/models/data.ts
+++ lib/src/models/data.ts
@@ -16,9 +16,7 @@
 * under the License.
 */
 
-import { OIDCEndpoints, OIDCProviderMetaData } from ".";
-import { AuthClientConfig } from "./client-config";
-
+import { OIDCEndpoints } from "./oidc-provider-meta-data";
 export type StoreValue = string | string[] | boolean | number | OIDCEndpoints;
 export type TemporaryData = { [ key: string ]: StoreValue; };
 
@@ -30,33 +28,6 @@ export interface SessionData {
     refresh_token?: string;
     token_type: string;
     session_state: string;
-}
-
-export interface DataLayer<T> {
-    setSessionData(sessionData: SessionData): Promise<void>;
-    setOIDCProviderMetaData(oidcProviderMetaData: OIDCProviderMetaData): Promise<void>;
-    setConfigData(config: AuthClientConfig<T>): Promise<void>;
-    setTemporaryData(data: TemporaryData): Promise<void>;
-    getSessionData(): Promise<SessionData>;
-    getOIDCProviderMetaData(): Promise<OIDCProviderMetaData>;
-    getConfigData(): Promise<AuthClientConfig<T>>;
-    getTemporaryData(): Promise<{ [key: string]: StoreValue }>;
-    removeSessionData(): Promise<void>;
-    removeOIDCProviderMetaData(): Promise<void>;
-    removeConfigData(): Promise<void>;
-    removeTemporaryData(): Promise<void>;
-    setSessionDataParameter(key: keyof SessionData, value: StoreValue): Promise<void>;
-    setOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData, value: StoreValue): Promise<void>;
-    setConfigDataParameter(key: keyof AuthClientConfig<T>, value: StoreValue): Promise<void>;
-    setTemporaryDataParameter(key: string, value: StoreValue): Promise<void>;
-    getSessionDataParameter(key: keyof SessionData): Promise<StoreValue>;
-    getOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): Promise<StoreValue>;
-    getConfigDataParameter(key: keyof AuthClientConfig<T>): Promise<StoreValue>;
-    getTemporaryDataParameter(key: string): Promise<StoreValue>;
-    removeSessionDataParameter(key: keyof SessionData): Promise<void>;
-    removeOIDCProviderMetaDataParameter(key: keyof OIDCProviderMetaData): Promise<void>;
-    removeConfigDataParameter(key: keyof AuthClientConfig<T>): Promise<void>;
-    removeTemporaryDataParameter(key: string): Promise<void>;
 }
 
 export interface Store {
