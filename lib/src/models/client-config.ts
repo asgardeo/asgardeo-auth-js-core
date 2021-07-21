32a3ce3d2617a638d234f1feb8e7699687198c79
NipuniBhagya <nipunib@wso2.com>
2021-07-21 15:02:51 +0530

    Add config to enable http request retry

--- lib/src/models/client-config.ts
+++ lib/src/models/client-config.ts
@@ -38,6 +38,11 @@ export interface StrictAuthClientConfig {
      * Allowed leeway for id_tokens (in seconds).
      */
     clockTolerance?: number;
+    /**
+     * The flag to specify whether https request retry
+     * should be enabled or not.
+     */
+    isHttpRequestRetryEnabled?: boolean;
 }
 
 export type AuthClientConfig<T = unknown> = StrictAuthClientConfig & T;
