0d121f1221e752c55eb94f99b511c3abb8bdbfd6
Theviyanthan <theviyanthan@outlook.com>
2022-02-28 23:13:48 +0530

    Add util functions to extract PKCE key from state param and to generate state param

--- lib/src/utils/authentication-utils.ts
+++ lib/src/utils/authentication-utils.ts
@@ -16,11 +16,12 @@
  * under the License.
  */
 
+import { PKCE_CODE_VERIFIER, PKCE_SEPARATOR } from "../constants";
 import { DecodedIDTokenPayload } from "../models";
 
 export class AuthenticationUtils {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    private constructor() { }
+    private constructor() {}
 
     public static filterClaimsFromIDTokenPayload(payload: DecodedIDTokenPayload): any {
         const optionalizedPayload: Partial<DecodedIDTokenPayload> = { ...payload };
@@ -41,7 +42,7 @@ export class AuthenticationUtils {
         delete optionalizedPayload?.sid;
 
         const camelCasedPayload = {};
-        Object.entries(optionalizedPayload).forEach(([ key, value ]) => {
+        Object.entries(optionalizedPayload).forEach(([key, value]) => {
             const keyParts = key.split("_");
             const camelCasedKey = keyParts
                 .map((key: string, index: number) => {
@@ -49,11 +50,11 @@ export class AuthenticationUtils {
                         return key;
                     }
 
-                    return [ key[ 0 ].toUpperCase(), ...key.slice(1) ].join("");
+                    return [key[0].toUpperCase(), ...key.slice(1)].join("");
                 })
                 .join("");
 
-            camelCasedPayload[ camelCasedKey ] = value;
+            camelCasedPayload[camelCasedKey] = value;
         });
 
         return camelCasedPayload;
@@ -69,7 +70,7 @@ export class AuthenticationUtils {
 
         // This works only when the email is used as the username
         // and the tenant domain is appended to the`sub` attribute.
-        return tokens.length > 2 ? tokens[ tokens.length - 1 ] : "";
+        return tokens.length > 2 ? tokens[tokens.length - 1] : "";
     };
 
     public static getTokenRequestHeaders(): HeadersInit {
@@ -77,5 +78,25 @@ export class AuthenticationUtils {
             Accept: "application/json",
             "Content-Type": "application/x-www-form-urlencoded"
         };
+    }
+
+    /**
+     * This generates the state param value to be sent with an authorization request.
+     *
+     * @param {string} pkceKey The PKCE key.
+     * @param {string} state The state value to be passed. (The correlation ID will be appended to this state value.)
+     *
+     * @returns {string} The state param value.
+     */
+    public static generateStateParamForRequestCorrelation(pkceKey: string, state?: string): string {
+        const index: number = parseInt(pkceKey.split(PKCE_SEPARATOR)[ 1 ]);
+
+        return state ? `${ state }_request_${ index }` : `request_${ index }`;
+    }
+
+    public static extractPKCEKeyFromStateParam(stateParam: string): string {
+        const index: number = parseInt(stateParam.split("request_")[ 1 ]);
+
+        return `${ PKCE_CODE_VERIFIER }${PKCE_SEPARATOR}${ index }`;
     }
 }
