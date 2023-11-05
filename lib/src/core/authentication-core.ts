3b770b3ac52963ebe7f9809212a93dfcf0f7ff74
Nipun Thennakoon <nipunsampath@gmail.com>
2023-11-05 23:08:24 +0530

    Properly URL encoding the sign-out URL.

--- lib/src/core/authentication-core.ts
+++ lib/src/core/authentication-core.ts
@@ -556,8 +556,8 @@ export class AuthenticationCore<T> {
                 "No sign-in redirect URL has been found either. "
             );
         }
-
-        let parameter: string = `client_id=${ configData.clientID }`;
+        const queryParams = new URLSearchParams();
+        queryParams.set("post_logout_redirect_uri", callbackURL);
 
         if (configData.sendIdTokenInLogoutRequest) {
             const idToken: string = (await this._dataLayer.getSessionData(userID))?.id_token;
@@ -569,16 +569,13 @@ export class AuthenticationCore<T> {
                     "No ID token could be found. Either the session information is lost or you have not signed in."
                 );
             }
-            parameter = `id_token_hint=${ idToken }`;
+            queryParams.set("id_token_hint", idToken);
+        } else {
+            queryParams.set("client_id", configData.clientID);
         }
 
-        const logoutCallback: string =
-            `${ logoutEndpoint }?` +
-            parameter +
-            `&post_logout_redirect_uri=${ callbackURL }&state=` +
-            SIGN_OUT_SUCCESS_PARAM;
-
-        return logoutCallback;
+        queryParams.set("state", SIGN_OUT_SUCCESS_PARAM);
+        return `${logoutEndpoint}?${queryParams.toString()}`;
     }
 
     public async clearUserSessionData(userID?: string): Promise<void> {
